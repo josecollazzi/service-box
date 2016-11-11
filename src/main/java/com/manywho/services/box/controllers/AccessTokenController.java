c4c390e9e789f8140f496fded5309e671ad8ce71
jose <jose.collazzi@manywho.com>
2016-11-11 16:52:16 +0000

    add user app token to get access token

--- src/main/java/com/manywho/services/box/controllers/AccessTokenController.java
+++ src/main/java/com/manywho/services/box/controllers/AccessTokenController.java
@@ -1,5 +1,6 @@
 package com.manywho.services.box.controllers;
 
+import com.box.sdk.BoxDeveloperEditionAPIConnection;
 import com.manywho.sdk.entities.run.EngineValue;
 import com.manywho.sdk.entities.run.EngineValueCollection;
 import com.manywho.sdk.entities.run.elements.config.ServiceRequest;
@@ -10,7 +11,10 @@ import com.manywho.sdk.services.annotations.AuthorizationRequired;
 import com.manywho.sdk.services.controllers.AbstractController;
 import com.manywho.services.box.client.BoxClient;
 import com.manywho.services.box.entities.Credentials;
+import com.manywho.services.box.facades.BoxFacadeInterface;
+import com.manywho.services.box.managers.CacheManagerInterface;
 import com.manywho.services.box.managers.TaskManager;
+import org.apache.commons.lang3.StringUtils;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
@@ -27,15 +31,30 @@ public class AccessTokenController extends AbstractController {
     @Inject
     private BoxClient boxClient;
 
+    @Inject
+    private CacheManagerInterface cacheManager;
+
+    @Inject
+    private BoxFacadeInterface boxFacade;
+
     @Path("/access-token")
     @POST
     @AuthorizationRequired
     public ServiceResponse getAccessToken(ServiceRequest serviceRequest) throws Exception {
         Credentials credentials = boxClient.getLastCredentials(getAuthenticatedWho().getToken());
 
+        String userAppId = cacheManager.getContextToUserApp(serviceRequest.getStateId());
+
+        String token = credentials.getAccessToken();
+
+        if (!StringUtils.isEmpty(userAppId)) {
+            BoxDeveloperEditionAPIConnection connection = boxFacade.createDeveloperApiUserConnection(userAppId);
+            token = connection.getAccessToken();
+        }
+
         ServiceResponse serviceResponse = new ServiceResponse();
         EngineValueCollection engineValues = new EngineValueCollection();
-        engineValues.add(new EngineValue("Access Token", ContentType.String, credentials.getAccessToken()));
+        engineValues.add(new EngineValue("Access Token", ContentType.String, token));
         serviceResponse.setOutputs(engineValues);
         serviceResponse.setToken(serviceRequest.getToken());
         serviceResponse.setInvokeType(InvokeType.Forward);
